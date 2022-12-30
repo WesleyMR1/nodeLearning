bba99a477b5df71302f4f08684f08a91ed5d70db
WesleyMR1 <WesleyMR012@gmail.com>
2022-12-30 13:31:45 -0300

    criar atualizar deletar

--- modules/express.js
+++ modules/express.js
@@ -4,12 +4,13 @@ const port = 8080;
 const UserModel = require("../src/models/user.model");
 
 const app = express();
-
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log(`Request Type: ${req.method}`);
+  console.log(`Content Type: ${req.headers["content-type"]}`);
+  console.log(`Data: ${new Date()}`);
 
-app.get("/home", (req, res) => {
-  res.contentType("application/html");
-  res.status(200).send("<h1>Hello World!</h1>");
+  next();
 });
 
 app.get("/users", async (req, res) => {
@@ -19,6 +20,38 @@ app.get("/users", async (req, res) => {
     res.status(200).json(users);
   } catch (error) {
     return res.status(500).send(error.message);
+  }
+});
+
+app.get("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await UserModel.findById(id);
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+
+app.patch("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await UserModel.findByIdAndUpdate(id, req.body, { new: true });
+
+    res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await UserModel.findByIdAndRemove(id);
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
   }
 });
 
